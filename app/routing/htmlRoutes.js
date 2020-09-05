345c92853f612313f9c615fca6288230ee157833
Mone E <mone.eunique12@gmail.com>
2020-09-05 09:30:11 -0700

    fixing classes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,10 +6,10 @@ module.exports = function (app) {
 
 app.get("/survey", function(req, res) {
   
-    res.sendFile(path.join(__dirname, "../views/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 }
