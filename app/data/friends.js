816f25112dae933bb86409c39f8ec7cae552f73c
Mone E <mone.eunique12@gmail.com>
2020-10-04 10:43:34 -0700

    updated match images

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,30 +5,36 @@ var friends = [
         scores:[1,1,1,1,1,1,1,1,1,1]
     },
     {
-        name:"The Grinch",
-        photo:"https://www.indiewire.com/wp-content/uploads/2018/02/shutterstock_5875878f.jpg?w=780",
+        name:"Gary",
+        photo:"https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         scores:[5,4,1,2,5,5,3,5,2,1]
     },
     {
-        name:"Mary Katherine Gallagher",
-        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMoVjuzfMYAS6gFP3ERXF4wX19_gIlNGpBwQ1D9_-Et240xja11Q&s",
+        name:"Bob",
+        photo:"https://www.nekoosalibrary.com/sites/www.nekoosalibrary.com/files/images/Cletus.jpg",
         scores:[3,1,4,5,5,1,2,2,4,1]
     },
     {
-        name:"McLovin",
-        photo:"https://i3.ytimg.com/vi/vocTWwjiq60/mqdefault.jpg",
+        name:"Sasha",
+        photo:"https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
         scores:[2,3,4,5,1,2,3,4,5,1]
     },
     {
-        name:"Cady Heron",
-        photo:"https://pngimage.net/wp-content/uploads/2018/05/cady-heron-png-1.png",
+        name:"Vicky",
+        photo:"https://images.pexels.com/photos/1308238/pexels-photo-1308238.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
         scores:[1,1,1,5,5,1,5,1,2,1]
     },
     {
-        name:"Regina George",
-        photo:"https://i.pinimg.com/originals/ab/79/d1/ab79d1eb0c2c5220beb2751928c773b2.jpg",
+        name:"Joey",
+        photo:"https://images.pexels.com/photos/1863170/pexels-photo-1863170.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
         scores:[5,1,1,5,1,1,4,1,2,3]
     },  
+
+    {
+        name:"Ashley",
+        photo:"https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        scores:[5,5,5,5,5,5,5,5,5,5]
+    }  
 ];
 
 module.exports = friends;
